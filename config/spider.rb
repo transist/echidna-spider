a2a277bed4b25521da70c04ab1b520dabe6ab659
Rainux Luo <rainux@gmail.com>
2013-02-28 16:03:20 +0800

    Use :hiredis drvier to avoid crash in EM.defer operation #5

--- config/spider.rb
+++ config/spider.rb
@@ -4,7 +4,7 @@ $redis = EventMachine::Synchrony::ConnectionPool.new(size: 2) do
   Redis::Namespace.new(
     ENV['ECHIDNA_REDIS_NAMESPACE'],
     redis: SymbolizedRedis.new(
-      host: ENV['ECHIDNA_REDIS_HOST'], port: ENV['ECHIDNA_REDIS_PORT'], driver: :synchrony
+      host: ENV['ECHIDNA_REDIS_HOST'], port: ENV['ECHIDNA_REDIS_PORT'], driver: :hiredis
     )
   )
 end
