eb1a908d10afac7aa9fbcf3f36543b3ec6242e8f
Rainux Luo <rainux@gmail.com>
2013-03-05 14:18:25 +0800

    Use filesystem as cache storage

--- lib/models/tencent_agent/api_response_cacher.rb
+++ lib/models/tencent_agent/api_response_cacher.rb
@@ -2,21 +2,45 @@ class TencentAgent
   module ApiResponseCacher
     extend ActiveSupport::Concern
 
-    CACHE_KEY = 'spider:tencent:api_cache'
-
     def cached_get(path, params = {}, &block)
-      cache_field = "GET #{path} #{params}"
+      cache_key = "GET #{path} #{params}"
+      cache_path = cache_path(cache_key)
 
-      if $redis.hexists(CACHE_KEY, cache_field)
-        $logger.notice log("Cache hit: #{cache_field}")
-        MultiJson.load($redis.hget(CACHE_KEY, cache_field))
+      if cache_exists?(cache_path)
+        $logger.notice log("Cache hit: #{cache_key}")
+        load_cache(cache_path)
 
       else
-        $logger.notice log("Cache miss: #{cache_field}")
-        result = get(path, params, &block)
-        $redis.hset(CACHE_KEY, cache_field, MultiJson.dump(result))
-        result
+        $logger.notice log("Cache miss: #{cache_key}")
+        store_cache(cache_path, get(path, params, &block))
       end
+    end
+
+    private
+
+    def cache_path(cache_key)
+      sha1_hash = Digest::SHA1.hexdigest(cache_key)
+      $app_root.join('cache', 'tencent_api', sha1_hash[0..3], sha1_hash)
+    end
+
+    def cache_exists?(cache_path)
+      cache_path.file?
+    end
+
+    def load_cache(cache_path)
+      MultiJson.load(cache_path.read)
+    end
+
+    def store_cache(cache_path, cache_data)
+      cache_path.dirname.tap do |dir|
+        dir.mkpath unless dir.directory?
+      end
+
+      File.open(cache_path, 'w') do |file|
+        file.puts(MultiJson.dump(cache_data, pretty: true))
+      end
+
+      cache_data
     end
   end
 end
