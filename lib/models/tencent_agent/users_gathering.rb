e5880336dc62998e8862ef5e1bd265d064a97a87
simsicon <simsicon@gmail.com>
2013-03-06 11:25:37 +0800

    Handle error msg of list related actions

--- lib/models/tencent_agent/users_gathering.rb
+++ lib/models/tencent_agent/users_gathering.rb
@@ -116,14 +116,39 @@ class TencentAgent
     end
 
     def user_list
-      @list ||= post('api/list/get_list')['data']['info'].map{|el| el if el['name'] == LIST_NAME }.compact.first
-      @list ||= post('api/list/create', format: 'json', name: LIST_NAME, description: LIST_NAME, tag: 'echidna', access: '1')
+      if @list.nil?
+        result = post('api/list/get_list')
+        @list = handle_result(result, "Failed to get list") do |r|
+          r['data']['info'].find{|el| el['name'] == LIST_NAME }
+        end
+      end
+
+      if @list.nil?
+        result = post('api/list/create', format: 'json', name: LIST_NAME, description: LIST_NAME, tag: 'echidna', access: '1')
+        @list = handle_result(result, "Failed to create list")
+      end
 
       @list
     end
 
     def add_user_to_list username
-      post('api/list/add_to_list', format: 'json',  names: username, listid: user_list['listid'])
+      result = post('api/list/add_to_list', format: 'json',  names: username, listid: user_list['listid'])
+      handle_result(result, 'Failed to add user to list')
+    end
+
+    def handle_result(result, errmsg, &block)
+      ret = nil
+      if result["errcode"].to_s == "0"
+        if block_given?
+          ret = block.call(result)
+        else
+          ret = result['data']
+        end
+      else
+        $logger.err log("#{errmsg}, errcode: #{result["errcode"]}, msg: #{result["msg"]}")
+        ret = {}
+      end
+      ret
     end
   end
 end
